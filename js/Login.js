db3d85aac026c0141088e471cb2a195e7c185c65
Omega050 <mauro-jr50@hotmail.com>
2023-08-09 19:03:04 -0300

    Trying hard to get better at this

--- js/Login.js
+++ js/Login.js
@@ -1,27 +1,47 @@
-class Login {
-    static logado = false;
-    static matLogado = null;
-    static nomeLogado = null;
-    static acessoLogado = null;
-    static endpoint = "https://login-api.maurojr1.repl.co"
-    static login = (mat, pas) => {
-        this.endpoint += `?matricula=${mat}&senha=${pas}`
-        fetch(this.endpoint)
-            .then(res => res.json)
-            .then(res => {
-                if(res){
-                this.logado = true
-                this.matLogado = mat;
-                this.nomeLogado = res.nome;
-                this.acessoLogado = res.acesso;
-                console.log(res)
-                }
-            
-            else{
-                console.log("Usuário ou senha incorretos")
-            }
-            }
-            )
+function setCookie(name, value) {
+    document.cookie = name + "=" + value + "; path=/";
+  }
+  
+  function getCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+      var c = ca[i];
+      while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+      if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
     }
-}
-export { Login };
+    return null;
+  }
+  
+  function login() {
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+  
+    // Simulação de validação de usuário e senha
+    if (username === "admin" && password === "admin123") {
+      alert("Bem-vindo, administrador!");
+      setCookie("userType", "admin");
+      window.location.href = "admPage.html";
+    } else if (username === "usuario" && password === "usuario123") {
+      alert("Login bem-sucedido como usuário!");
+      setCookie("userType", "user");
+      window.location.href = "userPage.html";
+    } else {
+      alert("Usuário ou senha incorretos.");
+    }
+  }
+  
+  function logout() {
+    setCookie("userType", "");
+    window.location.href = "index.html";
+  }
+  
+  // Verificar o cookie para manter o usuário logado
+  document.addEventListener("DOMContentLoaded", function () {
+    var userType = getCookie("userType");
+    if (userType === "admin") {
+      window.location.href = "./admPage.html";
+    } else if (userType === "user") {
+      window.location.href = "./userPage.html";
+    }
+  });
